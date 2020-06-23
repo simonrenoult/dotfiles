84fc5f1e3d0bcbf01f0db023e46b474e6d52542b
Simon Renoult <contact@simonrenoult.me>
2020-06-23 14:34:32 +0200

    feat: add zsh-autosuggestions

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,9 @@
 export ZSH="$HOME/.oh-my-zsh"
+
 ZSH_THEME="avit"
-plugins=(git docker)
+
+plugins=(git docker zsh-autosuggestions)
 
 alias reload-zsh=". $DOTFILES"
 
-source $ZSH/oh-my-zsh.sh
+. $ZSH/oh-my-zsh.sh
