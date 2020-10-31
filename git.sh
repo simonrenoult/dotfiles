dbc661f4f02aa710bbd0d328fa2161c606145818
Simon Renoult <contact@simonrenoult.me>
2020-10-31 09:46:09 +0100

    feat(git): add new git aliases

--- git.sh
+++ git.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/zsh
 
-alias gitconfig="$EDITOR $HOME/.gitconfig"
-alias gitrc=gitconfig
-alias gitignore="$EDITOR $HOME/.gitignore"
+function branch {
+    if [ -z "$1" ]; then
+        echo "Error: an argument must be provided"
+    else
+        wip
+        git fetch --all > /dev/null
+        git checkout -b $1 origin/master
+    fi
+}
 
-alias branch='git checkout -b'
-alias commit='git commit --allow-empty --message'
-alias push='git push --force origin $(git rev-parse --abbrev-ref HEAD)'
-alias tags='git tag --list -n1'
-alias undo='git reset --soft HEAD~'
-alias nuke='git reset --hard && git clean --force -d'
-alias fixup='git add --all && git commit --fixup'
+function branches {
+    git for-each-ref \
+        --sort=committerdate refs/heads/ \
+        --format='%(color: cyan)%(committerdate:short) %(color: blue)%(refname:short)'
+}
+
+
+function commit {
+    if [ -z "$1" ]; then
+        echo "Error: an argument must be provided"
+    else
+        git commit --allow-empty --message=$1
+    fi
+}
+
+
+function current_branch {
+    git rev-parse --abbrev-ref HEAD
+}
+
 
 function debranch {
     if [ -z "$1" ]; then
@@ -23,20 +42,56 @@ function debranch {
     fi
 }
 
-function branch-from-master {
-    if [ -z "$1" ]; then
-        echo "Error: an argument must be provided"
-    else
-        wip
-        git fetch --all > /dev/null
-        git checkout -b $1 origin/master
-    fi
-}
 
 function melt {
     git add --all
     git commit --amend --no-edit
 }
+
+
+function news {
+    git fetch --all > /dev/null
+    git --no-pager log --oneline --decorate --first-parent origin/master..HEAD
+}
+
+
+function rebase-on-master {
+    git fetch --all > /dev/null
+    git rebase origin/master
+}
+
+
+function rebase-on-remote {
+    local remote_branch=$(git rev-parse --abbrev-ref HEAD)
+    git fetch --all > /dev/null
+    git rebase origin/$remote_branch
+}
+
+
+function reset-to-master {
+    git fetch --all > /dev/null
+    git reset --hard origin/master
+}
+
+
+function reset-to-remote {
+    local remote_branch=$(git rev-parse --abbrev-ref HEAD)
+    git fetch --all > /dev/null
+    git reset --hard origin/$remote_branch
+}
+
+
+function squash-all {
+    git fetch --all > /dev/null
+    git rebase --interactive --autosquash origin/master
+}
+
+
+function wip {
+    git add --all
+    git commit --message="wip: undo me with 'git reset --soft HEAD~'"
+}
+
 
 function merged {
     if [ $# -eq 0 ]; then
@@ -48,28 +103,28 @@ function merged {
     fi
 }
 
-function news {
-    git fetch --all > /dev/null
-    git log --oneline --decorate --first-parent origin/master..HEAD
-}
 
-function rebase-on-master {
-    git fetch --all > /dev/null
-    git rebase origin/master
-}
+alias gitconfig="$EDITOR $HOME/.gitconfig"
+alias gitrc=gitconfig
+alias gitignore="$EDITOR $HOME/.gitignore"
 
-function reset-to-master {
-    git fetch --all > /dev/null
-    git reset --hard origin/master
-}
+alias push='git push --force origin $(git rev-parse --abbrev-ref HEAD)'
+alias tags='git tag --list -n1'
+alias undo='git reset --soft HEAD~'
+alias nuke='git reset --hard && git clean --force -d'
+alias fixup='git add --all && git commit --fixup'
+alias master='git checkout master'
+alias co='git checkout'
 
-function squash {
-    git fetch --all > /dev/null
-    git rebase --interactive --autosquash origin/master
-}
-
-function wip {
-    git add --all
-    git commit --message="wip: undo me with 'git reset --soft HEAD~'"
-}
-
+alias m=master
+alias b=branch
+alias cm=commit
+alias cb=current_branch
+alias db=debranch
+alias n=news
+alias rom=rebase-on-master
+alias rtm=reset-to-master
+alias rtr=reset-to-remote
+alias ror=rebase-on-remote
+alias sa=squash-all
+alias w=wip
